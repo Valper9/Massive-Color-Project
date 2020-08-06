12d8a3737cf98796f571a23f4203cb00b3064c4f
Valentin <val.detry@gmail.com>
2020-08-06 16:03:23 +0200

    Refactoring styles in JSS

--- src/App.js
+++ src/App.js
@@ -1,64 +1,67 @@
-import React,{Component} from 'react';
-import {Route, Switch} from "react-router-dom";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import PaletteList from './PaletteList';
 import Palette from './Palette';
 import NewPaletteForm from './NewPaletteForm';
 import seedColors from './seedColors';
-import {generatePalette} from './colorHelper';
+import { generatePalette } from './colorHelper';
 import SingleColorPalette from './SingleColorPalette';
 
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			palettes: seedColors
+		};
+		this.savePalette = this.savePalette.bind(this);
+		this.findPalette = this.findPalette.bind(this);
+	}
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      palettes: seedColors
-    };
-    this.savePalette = this.savePalette.bind(this);
-    this.findPalette = this.findPalette.bind(this);
-  }
+	findPalette(id) {
+		return this.state.palettes.find(function(palette) {
+			return palette.id === id;
+		});
+	}
 
-  findPalette(id){
-  return this.state.palettes.find(function(palette){
-    return palette.id === id;
-  });
-  }
+	savePalette(newPalette) {
+		this.setState({ palettes: [ ...this.state.palettes, newPalette ] });
+	}
 
-  savePalette(newPalette){
-    this.setState({palettes: [...this.state.palettes, newPalette]})
-  }
-
-  render(){
-    return (
-      <Switch >
-        <Route excat path="/palette/new" render={(routeProps) => <NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps}/> } />
-        <Route exact path="/" render={(routeProps) => <PaletteList palettes={this.state.palettes} {...routeProps} /> } />
-        <Route 
-          exact 
-          path="/palette/:id" 
-          render={ routeProps => ( 
-            <Palette 
-              palette={generatePalette(
-                this.findPalette(routeProps.match.params.id)
-              )}
-            />
-          )}
-        />
-      <Route 
-        exact 
-        path="/palette/:paletteId/:colorId" 
-        render={ routeProps => ( 
-            <SingleColorPalette 
-              colorId={routeProps.match.params.colorId}
-              palette={generatePalette(
-                this.findPalette(routeProps.match.params.paletteId)
-              )}
-            />
-        )} 
-      />
-      </Switch>
-    );
-  }
+	render() {
+		return (
+			<Switch>
+				<Route
+					excat
+					path="/palette/new"
+					render={(routeProps) => (
+						<NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps} />
+					)}
+				/>
+				<Route
+					exact
+					path="/"
+					render={(routeProps) => <PaletteList palettes={this.state.palettes} {...routeProps} />}
+				/>
+				<Route
+					exact
+					path="/palette/:id"
+					render={(routeProps) => (
+						<Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+					)}
+				/>
+				<Route
+					exact
+					path="/palette/:paletteId/:colorId"
+					render={(routeProps) => (
+						<SingleColorPalette
+							colorId={routeProps.match.params.colorId}
+							palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
+						/>
+					)}
+				/>
+			</Switch>
+		);
+	}
 }
 
 export default App;
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -9,45 +9,8 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import { AppBar, Toolbar } from '@material-ui/core';
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-
-const drawerWidth = 400;
-
-const styles = (theme) => ({
-	root: {
-		display: 'flex'
-	},
-	appBar: {
-		transition: theme.transitions.create([ 'margin', 'width' ], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.leavingScreen
-		}),
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		height: '64px'
-	},
-	appBarShift: {
-		width: `calc(100% - ${drawerWidth}px)`,
-		marginLeft: drawerWidth,
-		transition: theme.transitions.create([ 'margin', 'width' ], {
-			easing: theme.transitions.easing.easeOut,
-			duration: theme.transitions.duration.enteringScreen
-		})
-	},
-	menuButton: {
-		marginRight: theme.spacing(2)
-	},
-	navBtns: {
-		marginRight: '1rem',
-		'& a': {
-			textDecoration: 'none'
-		}
-	},
-	buttons: {
-		margin: '0 0.5rem'
-	}
-});
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
+import styles from './styles/PaletteFormNavStyles';
 
 class PaletteFormNav extends Component {
 	constructor(props) {
@@ -90,9 +53,11 @@ class PaletteFormNav extends Component {
 							color="inherit"
 							aria-label="Open drawer"
 							onClick={this.props.handleDrawerOpen}
-							className={classNames(classes.menuButton, open && classes.hide)}
+							className={classNames(classes.menuButton, {
+								[classes.hide]: open
+							})}
 						>
-							<MenuIcon />
+							<AddToPhotosIcon />
 						</IconButton>
 						<Typography variant="h6" color="inherit" noWrap>
 							Create A Palette
