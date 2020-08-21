8b2b47b1bc4cc756181dce30f20c1e3121c27465
Valentin <val.detry@gmail.com>
2020-08-21 11:56:54 +0200

    Optimizing with PureComponent

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -23,6 +23,7 @@ class PaletteList extends Component {
 		this.openDialog = this.openDialog.bind(this);
 		this.closeDialog = this.closeDialog.bind(this);
 		this.handleDelete = this.handleDelete.bind(this);
+		this.goToPalette = this.goToPalette.bind(this);
 	}
 	handleDelete() {
 		this.props.deletePalette(this.state.deleteId);
@@ -51,7 +52,7 @@ class PaletteList extends Component {
 							<CSSTransition key={palette.id} classNames="fade" timeout={500}>
 								<MiniPalette
 									{...palette}
-									handleClick={() => this.goToPalette(palette.id)}
+									goToPalette={this.goToPalette}
 									// handleDelete={this.props.deletePalette}
 									openDialog={this.openDialog}
 									key={palette.id}
